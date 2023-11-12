4024629c88d42c30af65bbae918cc43a40a52156
gschlosser <gschlossers@aol.com>
2023-11-12 14:48:23 -0800

    Success sending hashed password to DB. commit from mingw64

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for, redirect, request
 import json
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy, table
 import os
 from marshmallow import Schema
 
+from flask_sqlalchemy.__init__ import __getattr__
+from flask_bcrypt import Bcrypt
+
+
+#import hashlib or better hashing package
+
+
+
+
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 url = os.getenv('DATABASE_URL')
 
@@ -14,20 +23,26 @@ app.config['SECRET_KEY'] = 'secretkey'
 
 db = SQLAlchemy(app)
 
-#def get_db_connection():
-    #conn = sqlite3.connect('moneybase.db')
-    #conn.row_factory = sqlite3.Row
-    #return conn
+
+'''def get_db_connection():
+    conn = sqlite3.connect('moneybase.db')
+    conn.row_factory = sqlite3.Row
+    return conn'''
+    
+with app.app_context():
+    db.Session()
 
 class User(db.Model):
+
     __tablename__ = 'moneyusers'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    
 
     def __init__(self, username, password):
         self.username=username
-        self.password=password
+        self.password=bcrypt.generate_password_hash(password).decode('utf-8')
 
 #app.context().push() with this is goated for connecting db
 with app.app_context():
@@ -38,15 +53,18 @@ with app.app_context():
 def hmpg():
     return render_template('home.html')
 
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    #if request.method == ['POST']:
-        #username=request.form['Username']
-        #password=request.form['Password']
-    #user=User(username, password)
-    #db.session.add(user)
-    #db.session.commit()
-    return render_template('submit.html')
+    render_template('register.html')
+    if request.method == ['POST']:
+        username=request.form['Username']
+        password=request.form['Password']
+        user=User(username, password)
+        db.session.add(user)
+        db.session.commit()
+        return Flask.redirect('/submit')
+    
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
@@ -54,10 +72,13 @@ def submit():
         username=request.form['username']
         password=request.form['password']
         
-    
-    user=User(username, password)
-    db.session.add(user)
-    db.session.commit()
+        #pass hashed pass to user like user=User(username, hashpw) or something
+        #we're gonna bcrypt   
+
+        user=User(username, password)
+        
+        db.session.add(user)
+        db.session.commit()
 
 
 #@app.route('/getposts')
