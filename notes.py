83f6cc5137450cc4f916e1391f4660c7703d7ee3
gschlosser <gschlossers@aol.com>
2023-11-14 16:36:18 -0800

    cmd push

--- notes.py
+++
@@ -1,8 +0,0 @@
-'''11/9 database still accepting logins into moneyusers table. working on hashing passwords
-    with bcrypt. thoughts: do we encrypt string right away at transport from login form? 
-                            do we take string literal as a variable then pass that to a new 
-                            variable to encrypt? first one seems best but with possible complications
-                            dehashing.
-                to do: get pgadmin password out of app.py, implement bcrypt'''
-'''11/10 bcrypt hashing properly. maybe change max hashing length from 80? have to NEST TO CHECK DB FOR 
-                                                                    EXISTING USER n return t/f'''
