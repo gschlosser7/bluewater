d0e191b734b2421b63a1401665f15100884a0e54
gschlosser <gschlossers@aol.com>
2024-07-07 17:23:54 -0700

    wsgi gunicorn

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
 from app import create_app
 
 if __name__ == '__main__':
-    create_app = create_app()
-    create_app.run()
+    application = create_app()
+    application.run()
     #app.run()
 else:
     gunicorn_app = create_app()
